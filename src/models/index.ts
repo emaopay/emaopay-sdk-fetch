926d2ca004e7f590be7dd65adf7cd9e1f10f0cb1
github-sdk-update <github-sdk-update@mashangjiama.com>
2024-05-01 00:12:41 +0800

    update

---
+++ src/models/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './EmaopayCreateMerchantParams';
+export * from './EmaopayCreateMonitorParams';
+export * from './EmaopayCreateNewMonitorParams';
+export * from './EmaopayCreateOrderParams';
+export * from './EmaopayCreateQrCodeParams';
+export * from './EmaopayCredentials';
+export * from './EmaopayDeleteQrCodeParams';
+export * from './EmaopayLoginParams';
+export * from './EmaopayLoginResponse';
+export * from './EmaopayMerchant';
+export * from './EmaopayMonitor';
+export * from './EmaopayMonitorMessage';
+export * from './EmaopayMonitorMessageParams';
+export * from './EmaopayOrder';
+export * from './EmaopayPagedResponseEmaopayMonitorMessage';
+export * from './EmaopayPagedResponseEmaopayOrder';
+export * from './EmaopayPagedResponseEmaopayQrCode';
+export * from './EmaopayQrCode';
+export * from './EmaopayRegisterParams';
+export * from './EmaopayUploadCredits';
+export * from './EmaopayUser';
